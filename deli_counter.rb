d3a121f60ea2361fab94e65c4ae1c471207766bf
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-08 21:11:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,6 @@ def line(deli)
   if deli.length == 0 
     puts "The line is currently empty."
   else
-    deli.each_with_index { |name, index| puts "The line is currently: #{index}.#{name}"}
+    deli.each.with_index { |name, index| puts "The line is currently: #{index}. #{name}"}
   end
 end
