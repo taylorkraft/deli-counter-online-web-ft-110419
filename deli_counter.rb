ca7ee933dde4d8a387e1de3d58ed58f2886ec04e
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-08 19:33:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 katz_deli = []
-ppl_in_line = 0
+num_in_line = 0
 
 
 def line(deli)
- if katz_deli > ppl_in_line
-   puts ""
+ if katz_deli > num_in_line
+   puts "The line is currently: #{katz_deli[num_in_line]}. "
  else 
    puts "The line is currently empy."
   end 
