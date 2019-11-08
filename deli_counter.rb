850b70099be879b076c65a03bb13a5ec3c387810
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-08 19:32:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,9 @@ ppl_in_line = 0
 
 
 def line(deli)
- if ppl_in_line > 
-   puts "The line is currently empty."
-   
+ if katz_deli > ppl_in_line
+   puts ""
+ else 
+   puts "The line is currently empy."
+  end 
 end
