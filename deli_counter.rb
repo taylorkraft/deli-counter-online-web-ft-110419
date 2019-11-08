636b3e526b1ca590f5e0ec27ff27440f6a0d8bdb
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-08 20:07:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,8 @@
 def line
   katz_deli = []
   counter = 0
-  while katz_deli == counter
+  katz_deli == counter
   puts "The line is currently empty."
-  katz_deli.each do |name|
-    puts "The line is currently: #{counter}. #{name} "
+  katz_deli.each {|name| puts "The line is currently: #{counter}. #{name} "}
  end
 end
