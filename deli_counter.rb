c638f115ed8b84fcae714297755e146a8ec2cf43
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-08 20:58:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(deli)
   if deli.length == 0 
     puts "The line is currently empty."
   else
-    puts "The line is currently #{counter}. #{name} "
+    puts "The line is currently #{|counter, name|}."
   end
   counter += 1
 end
