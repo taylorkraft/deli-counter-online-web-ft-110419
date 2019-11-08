cc869ee026cd723684b9f96f6b0a85197ce71c41
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-08 21:07:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,6 @@ def line(deli)
   if deli.length == 0 
     puts "The line is currently empty."
   else
-    deli.each_with_index { |index, name| p "The line is currently: "#{index}
+    deli.each_with_index { |index, name| p "The line is currently: #{index}.#{name}"
   end
 end
